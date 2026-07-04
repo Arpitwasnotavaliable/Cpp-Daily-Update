c3138ec45524f0081cb96c8e67ada0d5205a5b16
Arpitwasnotavaliable <arpitsrivastava103@gmail.com>
2026-07-04 23:27:48 +0530

    Some practice Binary search questions

---
+++ Fst&lstOccurance.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc(int Arr[],int size,int key){
+        int start=0,end=size-1;
+        int ans=-1;
+        int mid=start+(end-start)/2;
+        while(start<=end){
+            if(Arr[mid]==key){
+                ans=mid;
+                end=mid-1;
+            }
+            else if(Arr[mid]<key){
+                start=mid+1;
+            }
+            else if(Arr[mid]>key){
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+            
+        }
+        return ans;
+}
+int lastOcc(int Arr[],int size,int key){
+        int start=0,end=size-1;
+        int ans=-1;
+        int mid=start+(end-start)/2;
+        while(start<=end){
+            if(Arr[mid]==key){
+                ans=mid;
+                start=mid+1;
+            }
+            else if(Arr[mid]<key){
+                start=mid+1;
+            }
+            else if(Arr[mid]>key){
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+            
+        }
+        return ans;
+}
+
+int main(){
+
+    int even[5]={1,2,3,3,5};
+
+    cout<<firstOcc(even,5,3);
+    cout<<lastOcc(even,5,3);
+
+
+
+    return 0;
+}
