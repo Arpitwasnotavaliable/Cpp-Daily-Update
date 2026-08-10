7c729a1452163125d356a3e3e8e148d003facc88
Arpitwasnotavaliable <arpitsrivastava103@gmail.com>
2026-08-10 20:59:16 +0530

    some minor changes

--- char/introchar.cpp
+++ char/introchar.cpp
@@ -1,6 +1,45 @@
 #include<iostream>
 using namespace std;
 
+char getMaxOccCharacter(string s){
+
+    int arr[26]={0};
+
+    for(int i=0;i<s.length(); i++){
+        char ch=s[i];
+        int number=0;
+            if(ch>='a'&&ch<='z'){
+                number=ch-'a';
+            }
+            else{
+                number=ch-'A';
+            }
+            arr[number]++;
+    }
+
+    int maxi=-1,ans=0;
+    for(int i=0;i<s.length();i++){
+            if(maxi<arr[i]){
+                ans=i;
+                maxi=arr[i];
+            }
+    }
+
+    char finalAns='a'+ans;
+    return finalAns;
+
+
+
+}
+
+
+
+
+
+
+
+
+
 int GetLen(char name[]){
     int count=0;
     for(int i=0;name[i]!='\0';i++){
